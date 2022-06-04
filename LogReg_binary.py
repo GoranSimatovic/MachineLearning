b5dff04c94f93934922263d16c16cf3d402fb1ff
GoranSimatovic <100073236+GoranSimatovic@users.noreply.github.com>
2022-06-04 18:54:23 +0200

    Add files via upload
    small edit

--- LogReg_binary.py
+++ LogReg_binary.py
@@ -10,9 +10,9 @@ import support_functions as sf
 data = pd.read_csv("wine.csv")
 data.dropna(inplace=True)
 
-#change labels into 0,1 integers (can be done with OneHot)
-data['quality'].replace('bad',0,inplace=True)
-data['quality'].replace('good',1,inplace=True)
+# change labels into 0,1 integers (can be done with OneHot)
+data['quality'].replace('bad', 0, inplace=True)
+data['quality'].replace('good', 1, inplace=True)
 
 target_variable = 'quality'
 list_drivers = data.columns.drop(target_variable).tolist()
@@ -22,11 +22,7 @@ target_data = data[target_variable]
 #min_max = MinMaxScaler()
 standard = StandardScaler()
 scaled_data = pd.DataFrame(standard.fit_transform(data[list_drivers]),
-                           columns = list_drivers, index = data.index)
-
-
-target_data = data[target_variable]
-
+                           columns=list_drivers, index=data.index)
 
 
 # split the data into test/train subsets (no validation)
@@ -41,59 +37,53 @@ train_y, test_y = train_test_split(target_data,
                                    random_state=state_n)
 
 
-
-#univariate analysis / feature significance
+# univariate analysis / feature significance
 univariate_results = sf.do_log_univariate(list_drivers,
-                                          train_x, test_x, 
+                                          train_x, test_x,
                                           train_y, test_y)
-                                          
 
 
-#pick up only features with gini > 5%
+# pick up only features with gini > 5%
 filtered_list = univariate_results\
-                .loc[univariate_results.GINI>0.05]\
-                .index.tolist()
+    .loc[univariate_results.GINI > 0.05]\
+    .index.tolist()
 
 
-
-#filter out highly correlated features
+# filter out highly correlated features
 corr_cutoff = 0.5
 filtered_list = sf.do_corr_filter(filtered_list,
                                   scaled_data[filtered_list],
                                   corr_cutoff)
-    
 
 
-#define several learning methodologies with different logic
 log_clf = LogisticRegression(random_state=0)
-
-
-
-
 log_clf.fit(train_x, train_y)
-print('#################\n'\
-+     'Full feature set:\n'\
-+     '#################')
+
+
+print('#################\n'
+      + 'Full feature set:\n'
+      + '#################')
 for i in list_drivers:
     print(list_drivers.index(i), i)
 print('\nAccuracy:',
-      round(accuracy_score(test_y, log_clf.predict(test_x)),2))
+      round(accuracy_score(test_y, log_clf.predict(test_x)), 2))
 print('GINI score:',
-      round(2*roc_auc_score(test_y, log_clf.predict(test_x))-1,2),
+      round(2*roc_auc_score(test_y, log_clf.predict(test_x))-1, 2),
       '\n\n')
 
 
 log_clf.fit(train_x[filtered_list], train_y)
 
-print('#####################\n'\
-+     'Filtered feature set:\n'\
-+     '#####################')
+print('#####################\n'
+      + 'Filtered feature set:\n'
+      + '#####################')
 for i in filtered_list:
-    print(filtered_list.index(i), i)   
+    print(filtered_list.index(i), i)
 print('\nAccuracy:',
-      round(accuracy_score(test_y, log_clf.predict(test_x[filtered_list])),2))
+      round(accuracy_score(test_y, log_clf.predict(test_x[filtered_list])), 2))
 print('GINI score:',
-      round(2*roc_auc_score(test_y, log_clf.predict(test_x[filtered_list]))-1,2),
+      round(2*roc_auc_score(test_y,
+                            log_clf.predict(test_x[filtered_list]))-1, 2),
       '\n\n')
 
 
@@ -103,30 +93,22 @@ print('Cool!')
 # * Search for best n-tuple of features
 # - performance is checked using k-fold cv and GINI
 
-log_clf = LogisticRegression(random_state=0, n_jobs = -1)
-
-import support_functions as sf
+log_clf = LogisticRegression(random_state=0, n_jobs=-1)
 
 best_three = sf.find_best_feature_set(log_clf, train_x[list_drivers],
-                                       train_y, 3, list_drivers, 5 )
-
+                                      train_y, 3, list_drivers, 5)
 
 show_first = 10
-plt.errorbar(x = range(show_first),
-             y = best_three['Gini'][:show_first],
-             yerr = best_three['std'][:show_first],
-             marker = 'o', linestyle= '--')
-plt.ylim([0.5,0.7])
+plt.errorbar(x=range(show_first),
+             y=best_three['Gini'][:show_first],
+             yerr=best_three['std'][:show_first],
+             marker='o', linestyle='--',
+             label=f'Best performer - {best_three.index[0]}')
+
+plt.ylim([0.5, 0.7])
 plt.xlabel('Features index')
 plt.ylabel('Gini [%]')
 plt.title('Model performance')
+plt.legend()
 plt.tight_layout()
 plt.show()
-
-
-
-
-
-
-
-
--- NN_titanic.py
+++ NN_titanic.py
@@ -1,10 +1,5 @@
-
-# Quick look up for Tensorflow 2 sequential model on titanic data
-
-
 from tensorflow import keras
 from tensorflow.keras import layers
-
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,8 +9,9 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 model = keras.Sequential()
-model.add(layers.Dense(units = 10, kernel_initializer='uniform', input_shape=(6,)))
+model.add(layers.Dense(units=10, kernel_initializer='uniform', input_shape=(6,)))
 model.add(layers.Activation('softmax'))
+model.add(layers.Dropout(0.2))
 model.add(layers.Dense(units=20))
 model.add(layers.Activation('relu'))
 model.add(layers.Dense(units=1))
@@ -26,29 +22,29 @@ model.compile(loss='categorical_crossentropy', optimizer=opt)
 
 
 data = pd.read_csv("titanic.csv")
-
 data = data.drop(['Name'], axis=1)
-data['Sex'].replace('female',0,inplace=True)
-data['Sex'].replace('male',1,inplace=True)
+data['Sex'].replace('female', 0, inplace=True)
+data['Sex'].replace('male', 1, inplace=True)
 
-scaler = MinMaxScaler(feature_range=(0,1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 data.astype('float64').dtypes
 data = scaler.fit_transform(data)
 
 train_set, test_set = train_test_split(data, test_size=0.2)
 
-train_set_labels = train_set[:,0]
-test_set_labels = test_set[:,0]
+train_set_labels = train_set[:, 0]
+test_set_labels = test_set[:, 0]
 
-train_set = train_set[:,1:]
-test_set = test_set[:,1:]
+train_set = train_set[:, 1:]
+test_set = test_set[:, 1:]
 
+# single run, no fancy stuff
 history = model.fit(train_set,
-                    train_set_labels, 
+                    train_set_labels,
                     batch_size=10,
-                    epochs = 25, 
-                    shuffle = True, 
-                    verbose = 3)
+                    epochs=25,
+                    shuffle=True,
+                    verbose=3)
 
 pd.DataFrame(history.history).plot(figsize=(8, 5))
 plt.grid(True)
